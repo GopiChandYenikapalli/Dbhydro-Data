8d5b46fd7cfb9a498857d3fbba5d964b1f74ce21
Gopi Chand Yenikapalli <gopichandyenikapalli@usf.edu>
2023-07-23 13:12:01 -0400

    added comments in code

--- test_hydro_parameters.R
+++ test_hydro_parameters.R
@@ -1,4 +1,4 @@
-
+# install library
 library(dbhydroR)
 library(data.table)
 # Call get_hydro function with dbkey_values
@@ -7,7 +7,7 @@ print(dbkeys_value)
 
 for (i in dbkeys_value) {
   hyd_data <- get_hydro(dbkey = i, date_min = "2000-01-01", date_max = format(Sys.Date(), "%Y-%m-%d"))
-  
+
   new_data <- data.frame(
     date = character(),
     station_id = character(),
@@ -16,7 +16,7 @@ for (i in dbkeys_value) {
     value = numeric(),
     stringsAsFactors = FALSE
   )
-  
+
   for (col in sort(colnames(hyd_data)[-1])) {
     parameter <- col
     data <- na.omit(data.frame(date = hyd_data$date, value = hyd_data[[col]], stringsAsFactors = FALSE))
@@ -27,12 +27,12 @@ for (i in dbkeys_value) {
     data <- data[, c("date", "station_id", "test_name", "unit", "value")]
     new_data <- rbind(new_data, data)
   }
-  
+
   new_data$date <- as.Date(new_data$date, format = "%m/%d/%Y")
   new_data <- new_data[order(new_data$date, new_data$test_name), ]
   new_data$date <- format(new_data$date, "%m/%d/%Y")
   new_data <- new_data[order(new_data$test_name), ]
-  
+
   file_name <- paste0("hyd_data_", unique(new_data$station_id), ".csv")
   write.csv(new_data, file = file_name, row.names = FALSE)
   # Add a delay of 3 seconds after each request
@@ -45,10 +45,12 @@ data <- get_hydro()
 print(data)
 # Extract information from the column name
 data <- data %>%
-  mutate(station_id = str_extract(colnames(.)[2], "^[^_]+"),
-         parameter = str_extract(colnames(.)[2], "(?<=_)[^_]+(?=_)"),
-         unit = str_extract(colnames(.)[2], "(?<=_)[^_]+$"),
-         value = L001_AIRT_Degrees_Celsius) %>%
+  mutate(
+    station_id = str_extract(colnames(.)[2], "^[^_]+"),
+    parameter = str_extract(colnames(.)[2], "(?<=_)[^_]+(?=_)"),
+    unit = str_extract(colnames(.)[2], "(?<=_)[^_]+$"),
+    value = L001_AIRT_Degrees_Celsius
+  ) %>%
   select(date, station_id, parameter, unit, value)
 
 # Save the modified output in a CSV file
